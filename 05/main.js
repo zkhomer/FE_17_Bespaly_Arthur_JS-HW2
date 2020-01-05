aec9a918c466d2ca353ba7b4cad739b604434538
Arthur <zkhomers@gmail.com>
2020-01-05 17:29:42 +0200

    5 task.

---
+++ 05/main.js
@@ -0,0 +1,6 @@
+var print1 = prompt("число 1 которое нужно разделить")
+var print2 = prompt("число 2 которое нужно разделить")
+var printof = prompt("на сколько хотите поделить")
+var resultat = print1 / printof
+var res2 = print2 / printof
+alert("Число 1 =" + Math.floor(resultat) + " " + "Число 2 =" + Math.floor(res2))
