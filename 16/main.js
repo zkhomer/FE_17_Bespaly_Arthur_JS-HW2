4da33fb2c2f99b7ff052eabd573196de5063aec8
Arthur <zkhomers@gmail.com>
2020-01-06 19:30:28 +0200

    Синий пояс Number: flats

---
+++ 16/main.js
@@ -0,0 +1,7 @@
+var floors = prompt("Веди количество этажей")
+var apartmentsPerFloor = prompt("веди количество квартир на этаже")
+var apartmentNumber = parseInt(prompt("Номер квартиры"))
+var floor = Math.ceil(apartmentNumber / apartmentsPerFloor)
+var porch = Math.ceil(floor / floors)
+floor -= (porch - 1) * floors
+alert("Квартира с номером " + apartmentNumber + " находится в подъезде № " + porch + " на " + floor + " этаже")
