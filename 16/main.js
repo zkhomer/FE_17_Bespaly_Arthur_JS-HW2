bc1afdd84e3468b642b18899f1db7d9cc39163bd
Arthur <zkhomers@gmail.com>
2020-01-07 22:26:57 +0200

    Синий пояс Number: flats

--- 16/main.js
+++ 16/main.js
@@ -1,6 +1,6 @@
 var floors = prompt("Веди количество этажей")
 var apartmentsPerFloor = prompt("веди количество квартир на этаже")
-var apartmentNumber = parseInt(prompt("Номер квартиры"))
+var apartmentNumber =prompt("Номер квартиры")
 var floor = Math.ceil(apartmentNumber / apartmentsPerFloor)
 var porch = Math.ceil(floor / floors)
 floor -= (porch - 1) * floors
