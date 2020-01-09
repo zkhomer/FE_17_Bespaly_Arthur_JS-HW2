c2f6713be1228e6fed239d0e9df1b561ce70bda0
Arthur <zkhomers@gmail.com>
2020-01-09 13:38:59 +0200

    size in

---
+++ 27/main.js
@@ -0,0 +1,19 @@
+var size = prompt("какой у вас размер ? ))) ")
+if (size == 40) {
+    alert("USA size = S")
+}
+else if (size == 42 || size == 44) {
+    alert("USA size = M")
+}
+else if (size == 46 || size == 48) {
+    alert("USA size = L")
+}
+else if (size == 50 || size == 52) {
+    alert("USA size = XL")
+}
+else if (size == 54) {
+    alert("USA size = XXL")
+}
+else {
+    alert("Please, write correct size.")
+}
--- 29/main.js
+++ 29/main.js
@@ -1,8 +1,7 @@
 var question = prompt("Please, write any number. For example: 10h - 10 hours; 10d - 10 days; 10w - 10 weeks; 10m - 10 months")
 if (question == "10h") {
     alert((parseFloat(question)) * 3600)
-}
-else if (question == "10d") {
+} else if (question == "10d") {
     alert((parseFloat(question)) * 24 * 3600)
 }
 if (question == "10w") {
